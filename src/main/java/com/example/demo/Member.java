443799d46dc73cc1bd0c836ca913bab7234afb59
saitomizuki <aoyami55@gmail.com>
2025-02-21 20:12:12 +0900

    ログインページ更新

---
+++ src/main/java/com/example/demo/Member.java
@@ -0,0 +1,13 @@
+
+package com.example.demo;
+public class Member {
+private int id;
+private String name;
+private String password;
+public Member() {};
+public Member(int id, String name, String password) {
+this.id =id;
+this.name = name;
+this.password = password;
+}
+}
